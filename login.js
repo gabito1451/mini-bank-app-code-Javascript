740fd52c55db48633ad0875def50ba2e424c8e61
Gabriel <gabrielobiagazie@gmail.com>
2023-01-30 20:10:44 +0100

    corrected login page

--- login.js
+++ login.js
@@ -1,32 +1,41 @@
 const passwordElem = document.getElementById("selected-user-password");
 const loginElem = document.querySelector(".login-btn");
 const selectElem = document.getElementById("select");
-
-let registeredUsers = JSON.parse(localStorage.getItem("MB_USER_ACCOUNTS"));
-for (let i = 0; i < registeredUsers.length; i++) {
-  let optionElem = document.createElement("option");
-  let text = document.createTextNode(registeredUsers[i].accountName);
-  optionElem.appendChild(text);
-  optionElem.setAttribute("value", registeredUsers[i].accountNumber);
-  selectElem.insertBefore(optionElem, selectElem.lastChild);
-}
+// if (localStorage.getItem("MB_LOGGEDIN_USER_ACCOUNT_NUMBER") !== null) {
+//   location.href = "transactions.html";
+// }
+let registeredUsers =
+  JSON.parse(localStorage.getItem("MB_USER_ACCOUNTS")) || [];
+registeredUsers.forEach((user) => {
+  const optionElem = document.createElement("option");
+  optionElem.value = user.accountNumber;
+  optionElem.textContent = `${user.accountName}(${user.accountPin})`;
+  passwordElem.textContent = `${user.accountPin}`;
+  selectElem.append(optionElem);
+});
 
 // function for logging in
 function login() {
-  localStorage.setItem(
-    "MB_LOGGEDIN_USER_ACCOUNT_NUMBER",
-    JSON.stringify(selectElem.value)
-  );
-  if (localStorage.getItem("MB_LOGGEDIN_USER_ACCOUNT_NUMBER") !== null) {
-    location.href = "transaction.html";
-  }
+  localStorage.setItem("MB_LOGGEDIN_USER_ACCOUNT_NUMBER", selectElem.value);
+
   //to check user object with the stored account number
-  let accountUserDetails = registeredUsers.find(
-    (acctNo) =>
-      acctNo.accountNumber ===
-      JSON.parse(localStorage.getItem("MB_LOGGEDIN_USER_ACCOUNT_NUMBER"))
+  const accountUserDetails = registeredUsers.find(
+    (user) =>
+      user.accountNumber ===
+      localStorage.getItem("MB_LOGGEDIN_USER_ACCOUNT_NUMBER")
   );
   console.log(accountUserDetails);
+  if (accountUserDetails.accountPin !== passwordElem.value) {
+    alert("incorrect pin");
+    return;
+  }
+
+  if (!accountUserDetails) {
+    alert("Account not found");
+    return;
+  }
+
+  location.href = "transactions.html";
 }
 
 loginElem.addEventListener("click", login);
