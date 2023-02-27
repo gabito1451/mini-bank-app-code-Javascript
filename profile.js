92cfad55f3042f8e104870b6f098a3190988f9b8
Gabriel <gabrielobiagazie@gmail.com>
2023-02-27 19:34:12 +0100

    made the necessary corrections

--- profile.js
+++ profile.js
@@ -7,56 +7,98 @@ const newPinElem = document.getElementById("new-pin");
 const confirmNewPinElem = document.getElementById("confirm-new-pin");
 const closeBtnElem = document.getElementById("close-btn");
 const updateBtnElem = document.getElementById("update-btn");
+const closeBtnConfirmationElem = document.getElementById(
+  "confirm-close-btn-action"
+);
+const yesCloseBtn = document.getElementById("yes");
+const dontCloseBtn = document.getElementById("no");
+
+const pinUpdateSection = document.getElementById("pin-update-section");
+
+const allUsers = getAllUsers();
+const currentUserIndex = getUserIndexByAccountNumber(currentUserAccountNumber);
 // display of aaccount number
 accountNumberElem.value = currentUserAccountNumber;
+accountNameElem.value = allUsers[currentUserIndex].accountName;
 
-const allUsers = JSON.parse(localStorage.getItem("MB_USER_ACCOUNTS"));
-
-function displayFormToUpdate() {
-  const pinUpdateSection = document.getElementById("pin-update-section");
-
-  if ((pinUpdateSection.style.display = "none")) {
+function togglePinChangeSectionDisplay() {
+  if (changePinElem.checked == true) {
     pinUpdateSection.style.display = "block";
+    currentPinElem.setAttribute("required", true);
+    newPinElem.setAttribute("required", true);
+    confirmNewPinElem.setAttribute("required", true);
   } else {
     pinUpdateSection.style.display = "none";
+    currentPinElem.setAttribute("required", false);
+    newPinElem.setAttribute("required", false);
+    confirmNewPinElem.setAttribute("required", false);
   }
 }
+
+changePinElem.addEventListener("click", () => togglePinChangeSectionDisplay());
+
 function updateForm() {
+  allUsers[currentUserIndex].accountName = accountNameElem.value;
+
+  // You need to check that user is updating PIN before you update it, otherwise, it will be updated to empty string
   if (
-    currentPinElem.value !==
-    getUserByAccountNumber(currentUserAccountNumber).accountPin
+    changePinElem.checked == true &&
+    currentPinElem.value !== allUsers[currentUserIndex].accountPin
   ) {
-    alert("Incorrect Pin");
+    alert("Pin Incorrect");
     return;
   }
-  if (newPinElem.value !== confirmNewPinElem.value) {
-    alert("Pins do not match, try again");
+  if (
+    changePinElem.checked == true &&
+    newPinElem.value !== confirmNewPinElem.value
+  ) {
+    alert("passwords do not match");
     return;
   }
+  if (
+    changePinElem.checked == true &&
+    confirmNewPinElem.value == allUsers[currentUserIndex].accountPin
+  ) {
+    alert("use a different password for update");
+  }
+  if (changePinElem.checked == true && newPinElem.value == " ") {
+    newPinElem.value = allUsers[currentUserIndex].accountPin;
+  }
+  if (
+    changePinElem.checked == true &&
+    newPinElem.value === confirmNewPinElem.value
+  ) {
+    allUsers[currentUserIndex].accountPin = newPinElem.value;
+  }
 
-  // finding the current user to update profile
-
-  let userToBeUpdated = allUsers.find(
-    (user) => user.accountNumber == accountNumberElem.value
-  );
-  userToBeUpdated.accountName = accountNameElem.value;
-  userToBeUpdated.accountPin = newPinElem.value;
-  // changing the updated properties to a string
-  const updatedObject = JSON.stringify(allUsers);
-  //  storing it back in local storage
-  localStorage.setItem("MB_USER_ACCOUNTS", updatedObject);
-  location.href = "transactions.html";
+  localStorage.setItem("MB_USER_ACCOUNTS", JSON.stringify(allUsers));
+  // location.href = "transactions.html";
 }
 updateBtnElem.addEventListener("click", () => updateForm());
 
-function deleteUserAccount() {
+function closeUserAccount() {
+  if ((closeBtnConfirmationElem.style.display = "none")) {
+    closeBtnConfirmationElem.style.display = "block";
+  }
+  closeBtnElem.style.display = "none";
+}
+closeBtnElem.addEventListener("click", () => closeUserAccount());
+
+function confirmCloseUserAccount() {
   const registeredUsers = localStorage.getItem("MB_USER_ACCOUNTS");
-  console.log(registeredUsers);
-  let usersArray = JSON.parse(registeredUsers);
+  const usersArray = JSON.parse(registeredUsers);
   const index = getUserIndexByAccountNumber(currentUserAccountNumber);
   usersArray.splice(index, 1);
-  const newArrayOfUsers = JSON.stringify(usersArray);
-  localStorage.setItem("MB_USER_ACCOUNTS", newArrayOfUsers);
-  // location.href = "/";
+  const remainingRegisteredUsers = JSON.stringify(usersArray);
+  localStorage.setItem("MB_USER_ACCOUNTS", remainingRegisteredUsers);
+  location.href = "/";
 }
-closeBtnElem.addEventListener("click", () => deleteUserAccount());
+yesCloseBtn.addEventListener("click", () => confirmCloseUserAccount());
+
+function doNotConfirmCloseUserAccount() {
+  if ((closeBtnConfirmationElem.style.display = "block")) {
+    closeBtnConfirmationElem.style.display = "none";
+  }
+  closeBtnElem.style.display = "block";
+}
+dontCloseBtn.addEventListener("click", () => doNotConfirmCloseUserAccount());
